0a6bc4cfd3c9464058e3243877cba5e99b9a0b12
Faisal Alhajari <faisalhejary@gmail.com>
2026-05-29 20:26:12 +0300

    added argo

--- infrastructure/scripts/install_ansible.sh
+++ infrastructure/scripts/install_ansible.sh
@@ -2,3 +2,8 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
+
+# Ansible <= 2.21 can't work with sudo-rs. 
+# for systems with sudo-rs you will to change to sudo  
+# sudo apt remove sudo-rs && sudo apt install sudo. 
+
